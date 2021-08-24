359cbe35fa887055e389334af9240bc85fb221e0
Evgeniy Khryashchev <e.khryashchev@outlook.com>
2021-08-24 21:28:34 +0500

    checking winning combinations

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -1,19 +1,35 @@
 package ru.job4j.puzzle;
 
 public class Win {
-    public static boolean check(int[][] board) {
-        int xWinConter = 0;
-
-        for (int[] field : board) {
-            int x = 0;
-            for (int column : field) {
-                if (column == 1) {
-                    xWinConter += x;
-                }
-                x += 1;
+    public static boolean monoHorizontal(int[][] board, int row) {
+        boolean result = true;
+        for (int i = 0; i < board.length; i++) {
+            if (board[row][i] != 1) {
+                result = false;
+                break;
             }
         }
+        return result;
+    }
+    public static boolean monoVertical(int[][] board, int column) {
+        boolean result = true;
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][column] != 1) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
 
-        return xWinConter % 5 == 0;
+    public static boolean check(int[][] board) {
+        boolean result = false;
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][i] == 1 && (monoHorizontal(board, i) || monoVertical(board, i))) {
+                result = true;
+                break;
+            }
+        }
+        return result;
     }
 }
